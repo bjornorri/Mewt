f539283cfdd24d0773a404265003393de340b3fe
Björn Orri Saemundsson <bjornorri@google.com>
2022-05-05 22:00:16 +0200

    Initial Commit

---
+++ Mewt/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Mewt
+//
+//  Created by Björn Orri Sæmundsson on 05.05.22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
